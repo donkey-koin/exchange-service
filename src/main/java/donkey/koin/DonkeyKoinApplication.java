d72ef925d5161559e98fd8f5f61f3f38c7d773e2
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-09 22:34:46 +0200

    Scaffolding: initial setup

---
+++ src/main/java/donkey/koin/DonkeyKoinApplication.java
@@ -0,0 +1,12 @@
+package donkey.koin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonkeyKoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonkeyKoinApplication.class, args);
+	}
+}
