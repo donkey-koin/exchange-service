30e851353699357c1643b7be8aa4ada2bae40151
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-11 20:18:47 +0200

    Setup relational database: added postgres to ci

--- src/main/java/donkey/koin/entities/user/User.java
+++ src/main/java/donkey/koin/entities/user/User.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "USER", schema = "exchange")
 public class User {
 
     @Id
