462626703d2f48b0e07d9566ac35adad61308116
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-12 17:59:11 +0200

    lombok magic

--- src/main/java/donkey/koin/exception/ResourceException.java
+++ src/main/java/donkey/koin/exception/ResourceException.java
@@ -1,17 +1,15 @@
 package donkey.koin.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 public class ResourceException extends RuntimeException {
 
+    @Getter
     private HttpStatus httpStatus;
 
     public ResourceException(HttpStatus httpStatus, String message) {
         super(message);
         this.httpStatus = httpStatus;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
     }
 }
