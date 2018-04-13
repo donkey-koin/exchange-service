a78bbffd13a2e49da4f70d355792819f82f4cf0c
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-13 19:16:33 +0200

    Implement registration logic: Added email

--- src/main/java/donkey/koin/entities/user/User.java
+++ src/main/java/donkey/koin/entities/user/User.java
@@ -23,4 +23,5 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private String email;
 }
--- src/main/java/donkey/koin/users/controller/UserRegistrationDetails.java
+++ src/main/java/donkey/koin/users/controller/UserRegistrationDetails.java
@@ -3,6 +3,7 @@ package donkey.koin.users.controller;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -19,9 +20,16 @@ public class UserRegistrationDetails {
     @Size(min = 6)
     private final String password;
 
+    @Email
+    @NotNull
+    @NotBlank
+    private final String email;
+
     public UserRegistrationDetails(@JsonProperty("username") String username,
-                                   @JsonProperty("password") String password) {
+                                   @JsonProperty("password") String password,
+                                   @JsonProperty("email") String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 }
