86b19d9e0d245eab1495dd925120b58183336442
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-16 22:05:15 +0200

    movement sweden

--- src/main/java/donkey/koin/entities/user/User.java
+++ src/main/java/donkey/koin/entities/user/User.java
@@ -5,21 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "USER", schema = "exchange")
+@Table(name = "USERS", schema = "exchange")
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,8 @@
 CREATE SCHEMA exchange;
+
+create table users (
+  id       numeric primary key not null,
+  username varchar(50)         not null,
+  password varchar(50)         not null,
+  email    varchar(50)         not null
+)
