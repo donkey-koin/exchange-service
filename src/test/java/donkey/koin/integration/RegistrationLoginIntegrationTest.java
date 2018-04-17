ab4aeae63a66620722fac1613823298785d4f811
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-18 00:08:11 +0200

    another testcase

--- src/test/java/donkey/koin/integration/RegistrationLoginIntegrationTest.java
+++ src/test/java/donkey/koin/integration/RegistrationLoginIntegrationTest.java
@@ -3,8 +3,11 @@ package donkey.koin.integration;
 import donkey.koin.app.DonkeyKoinApplication;
 import donkey.koin.dictionaries.WebServicesDictionary;
 import donkey.koin.users.registration.UserRegistrationService;
+import io.jsonwebtoken.SignatureException;
 import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.util.ResourceUtils;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -38,6 +42,10 @@ public class RegistrationLoginIntegrationTest {
     @Autowired
     private UserRegistrationService repository;
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    //TODO: more tests instead of one big integration, it's here only allow for any kind of programming
     @Test
     public void test() throws Exception {
         String registrationCredentials = FileUtils.readFileToString(ResourceUtils.getFile(REGISTRATION_CREDENTIALS));
@@ -62,10 +70,21 @@ public class RegistrationLoginIntegrationTest {
 
         String loginCredentials = FileUtils.readFileToString(ResourceUtils.getFile(LOGIN_CREDENTIALS));
 
-        mvc.perform(post("/login")
+        MvcResult authorizationResult = mvc.perform(post("/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(loginCredentials))
                 .andExpect(status().isOk())
-                .andExpect(header().exists("Authorization"));
+                .andExpect(header().exists("Authorization")).andReturn();
+
+        String authorizationJwtHeader = authorizationResult.getResponse().getHeader("Authorization");
+
+        mvc.perform(post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON).header("Authorization", authorizationJwtHeader))
+                .andExpect(status().isBadRequest());
+
+        exception.expect(SignatureException.class);
+        mvc.perform(post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON).header("Authorization", authorizationJwtHeader + "wrong"))
+                .andExpect(status().isForbidden());
     }
 }
