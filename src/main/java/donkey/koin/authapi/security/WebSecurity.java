4768143487e39959d0759bbcaa7c9d95e47c5acc
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-17 19:36:37 +0200

    simple changes

--- src/main/java/donkey/koin/authapi/security/WebSecurity.java
+++ src/main/java/donkey/koin/authapi/security/WebSecurity.java
@@ -1,6 +1,5 @@
 package donkey.koin.authapi.security;
 
-import donkey.koin.dictionaries.WebServicesDictionary;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -25,7 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, WebServicesDictionary.USERS).permitAll()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
