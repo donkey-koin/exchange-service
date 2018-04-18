5fcc57139563e6937ec866c210cc22c4ae28eb46
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-19 01:47:38 +0200

    gen sequence check

--- src/main/java/donkey/koin/entities/user/User.java
+++ src/main/java/donkey/koin/entities/user/User.java
@@ -5,7 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
@@ -16,7 +19,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue
     private Long id;
     private String username;
     private String password;
