926f2979085d8cf53a6cc011b071ca5d22b08bc7
Siorek, Oskar <oskar.siorek@sabre.com>
2018-04-19 01:36:09 +0200

    gen sequence check

--- src/main/java/donkey/koin/entities/user/User.java
+++ src/main/java/donkey/koin/entities/user/User.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String username;
     private String password;
