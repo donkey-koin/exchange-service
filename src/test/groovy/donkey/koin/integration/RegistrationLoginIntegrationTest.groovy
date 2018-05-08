226d50bb3224d71d250df44478be2430a17ebffa
Siorek, Oskar <oskar.siorek@sabre.com>
2018-05-08 17:50:26 +0200

    deterministic tests

--- src/test/groovy/donkey/koin/integration/RegistrationLoginIntegrationTest.groovy
+++ src/test/groovy/donkey/koin/integration/RegistrationLoginIntegrationTest.groovy
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.util.ResourceUtils
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -28,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         classes = ExchangeApplication)
 @AutoConfigureMockMvc
 @ActiveProfiles('dev')
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 class RegistrationLoginIntegrationTest {
 
     static String CLASSPATH = 'classpath:'
--- src/test/groovy/donkey/koin/integration/WalletIntegrationTest.groovy
+++ src/test/groovy/donkey/koin/integration/WalletIntegrationTest.groovy
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -23,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         classes = ExchangeApplication)
 @AutoConfigureMockMvc
 @ActiveProfiles('dev')
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 class WalletIntegrationTest {
 
     @Autowired
