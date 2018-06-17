0e9b5acabb1b7db6118ad5d27535f68bdd1389da
Siorek, Oskar <oskar.siorek@sabre.com>
2018-06-17 23:18:58 +0200

    test user

--- src/main/java/donkey/koin/app/ExchangeApplication.java
+++ src/main/java/donkey/koin/app/ExchangeApplication.java
@@ -1,10 +1,15 @@
 package donkey.koin.app;
 
+import donkey.koin.users.registration.UserRegistrationDetails;
+import donkey.koin.users.registration.UserRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.annotation.PostConstruct;
 
 @EntityScan(basePackages = {"donkey.koin.entities"})
 @EnableJpaRepositories(basePackages = {"donkey.koin.entities"})
@@ -12,7 +17,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class ExchangeApplication {
 
+    @Autowired
+    private UserRegistrationService registrationService;
+
     public static void main(String[] args) {
         SpringApplication.run(ExchangeApplication.class, args);
+    }
+
+    @PostConstruct
+    private void registerAdmin() {
+        registrationService.registerUser(new UserRegistrationDetails("a", "a", "a@gmail.com"));
     }
 }
